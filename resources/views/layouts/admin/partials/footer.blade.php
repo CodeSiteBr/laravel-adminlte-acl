bf2882fb7784d197d33b342848b538583aba81b1
Marcelo Camargo <celaummm@gmail.com>
2018-11-23 17:59:00 -0200

    Personalizado o footer

--- resources/views/layouts/admin/partials/footer.blade.php
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -3,5 +3,8 @@
     <div class="pull-right hidden-xs">
         <b>@lang('admin.version')</b> {{ config('adminlte.version') }}
     </div>
-    <strong>Copyright &copy; 2016-{{ Carbon\carbon::now()->year }} <a href="{{ route('home') }}">{{ env('APP_NAME') }}</a>.</strong> @lang('admin.all_rights_reserved')
+
+    <strong>Copyright &copy; {{ config('adminlte.since_year') }}-{{ Carbon\carbon::now()->year }}
+    <a href="{{ route('home') }}">{{ config('adminlte.footer_name') }}</a>.</strong>
+    @lang('admin.all_rights_reserved')
 </footer>
