4f3edc2b25e8ed8bdb70315d1c5d6b5c6f2095d0
Marcelo Camargo <celaummm@gmail.com>
2018-11-23 18:38:38 -0200

    add negrito no link

--- resources/views/layouts/admin/partials/footer.blade.php
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -4,7 +4,8 @@
         <b>@lang('admin.version')</b> {{ config('adminlte.version') }}
     </div>
     <div class="text-center">
-        • @lang('admin.all_rights_reserved') {{ config('adminlte.since_year') }}-{{ Carbon\carbon::now()->year }} &copy; <a href="{{ route('home') }}">{{ config('adminlte.footer_name') }}</a> •
-        @lang('admin.produced_by') <a href="{{ config('adminlte.produced_url') }}">{{ config('adminlte.produced_by') }}</a> •
+        • @lang('admin.all_rights_reserved') {{ config('adminlte.since_year') }}-{{ Carbon\carbon::now()->year }} &copy;
+        <a href="{{ route('home') }}"><b>{{ config('adminlte.footer_name') }}</b></a> •
+        @lang('admin.produced_by') <a href="{{ config('adminlte.produced_url') }}"><b>{{ config('adminlte.produced_by') }}</b></a> •
     </div>
 </footer>
